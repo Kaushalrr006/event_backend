0dd6403720eb0257ef70441286d844975ee8d45e
Shubham Dandge <shubham.dandge@Linkcode.in>
2023-09-08 18:49:30 +0530

    Delete Api

--- controllers/ToDoController.js
+++ controllers/ToDoController.js
@@ -37,3 +37,21 @@ module.exports.updateToDo = async(req, res)=>{
         res.status(500).send("Internal server error");
     }
 }
+module.exports.deleteToDo = async (req, res) => {
+    const {id}= req.params;
+    const { title, description } = req.body;
+
+    try{
+        const deleteToDo = await ToDoModel.findByIdAndDelete(id, {title, description},{new : true });
+        if(!deleteToDo){
+            return res.status(404).send("not found!!!!!!")
+        }
+        console.log('Sucessfully updated');
+        console.log('result : ',deleteToDo);
+        res.send(deleteToDo); 
+    }
+    catch (error) {
+        console.error("Error Updating ToDo:", error);
+        res.status(500).send("Internal server error");
+    }
+}
