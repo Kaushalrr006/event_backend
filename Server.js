029968269d5a200d9143ca38a2f4e52d8e81351e
sahillondhe3 <129080224+sahillondhe3@users.noreply.github.com>
2023-09-09 00:10:26 +0530

    corsCODEline

--- Server.js
+++ Server.js
@@ -10,11 +10,12 @@ require('dotenv').config()
 const app = express()
 const PORT = process.env.PORT || 5000
 app.use(express.json())
+app.use(cors())
 mongoose
 .connect(process.env.MONGODB_URL)
 .then(()=>console.log('Connected to MongoDB!'))
 .catch((err)=>console.log(err))
 
 app.use(routes)
-app.use(cors())
+
 app.listen(PORT,()=>console.log('listening on port 5000'))
