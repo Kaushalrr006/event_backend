1229ecca95be913ccbcd007726742f834e73d311
sahillondhe3 <129080224+sahillondhe3@users.noreply.github.com>
2023-09-09 14:58:21 +0530

    cors error fixed

--- Server.js
+++ Server.js
@@ -8,18 +8,16 @@ require('dotenv').config()
 
 
 const app = express()
+app.use(cors())
 const PORT = process.env.PORT || 5000
+
 app.use(express.json())
 
 mongoose
-.connect(process.env.MONGODB_URL)
-.then(()=>console.log('Connected to MongoDB!'))
-.catch((err)=>console.log(err))
+  .connect(process.env.MONGODB_URL)
+  .then(() => console.log('Connected to MongoDB!'))
+  .catch((err) => console.log(err))
 
 app.use(routes)
-app.use(cors({
-    origin: "*"
-  })
-  )
 
-app.listen(PORT,()=>console.log('listening on port 5000'))
+app.listen(PORT, () => console.log('listening on port 5000'))
