02b65f994e7ecced62b6d4c952253f6d063574c9
sahillondhe3 <129080224+sahillondhe3@users.noreply.github.com>
2023-09-18 22:07:48 +0530

    cleared the comment

--- controllers/ToDoController.js
+++ controllers/ToDoController.js
@@ -22,7 +22,7 @@ module.exports.createToDo = async (req, res) => {
     const { title, description } = req.body;
 
     try {
-        const data = await ToDoModel.create({ title, description });
+        const data = await ToDoModel.create({ title, description, status:"todo" });
         console.log("Created Successfully");
         console.log(data);
         res.send(data);
@@ -32,24 +32,6 @@ module.exports.createToDo = async (req, res) => {
     }
 };
 
-// module.exports.updateToDo = async(req, res)=>{
-//     const {id}= req.params;
-//     const { title, description } = req.body;
-
-//     try{
-//         const updateToDo = await ToDoModel.findByIdAndUpdate(id, {title, description},{new : true });
-//         if(!updateToDo){
-//             return res.status(404).send("not found!!!!!!")
-//         }
-//         console.log('Sucessfully updated');
-//         console.log('result : ',updateToDo);
-//         res.send(updateToDo); 
-//     }
-//     catch (error) {
-//         console.error("Error Updating ToDo:", error);
-//         res.status(500).send("Internal server error");
-//     }
-// }
 module.exports.updateToDo = async (req, res) => {
     const { id } = req.params;
     const { title, description, status } = req.body;
@@ -92,7 +74,7 @@ module.exports.updateToDoStatus = async (req, res) => {
 
 module.exports.deleteToDo = async (req, res) => {
     const {id}= req.params;
-    const { title, description } = req.body;
+    const { title, description} = req.body;
 
     try{
         const deleteToDo = await ToDoModel.findByIdAndDelete(id, {title, description},{new : true });
